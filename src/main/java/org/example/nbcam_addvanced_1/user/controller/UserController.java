671ac32284bbac7d4cdbdeb096d7616dfe20dec6
Viva <happybird00@naver.com>
2025-09-29 19:56:10 +0900

    챕터 1-1 : Filter 개념과 동작 원리

--- src/main/java/org/example/nbcam_addvanced_1/user/controller/UserController.java
+++ src/main/java/org/example/nbcam_addvanced_1/user/controller/UserController.java
@@ -1,2 +1,20 @@
-package org.example.nbcam_addvanced_1.user.controller;public class UserController {
+package org.example.nbcam_addvanced_1.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+
+    @GetMapping("/get")
+    public String getUserInfo() {
+        log.info("호출");
+        return "호출 되었습니다.";
+    }
 }
